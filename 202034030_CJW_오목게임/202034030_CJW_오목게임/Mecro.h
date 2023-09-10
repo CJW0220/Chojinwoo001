2e007ff713ead7c34efcd38964565c911dedf329
CJW0220 <127714591+CJW0220@users.noreply.github.com>
2023-09-10 21:31:17 +0900

    Delete 202034030_CJW_오목게임/202034030_CJW_오목게임 directory

--- 202034030_CJW_오목게임/202034030_CJW_오목게임/Mecro.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <conio.h>
-#include <string>
-#include <Windows.h>
-#include <list>
-#include <fstream>
-using namespace std;
--- 202034030_CJW_오목게임/202034030_CJW_오목게임/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameManager.h"
-
-void main()
-{
-	GameManager gameManager;
-	gameManager.OpenGame();
-}
